83a7f64405a8acbb990cb232b893db6d595b6f73
Emam saimon <likonsaimon123@gmail.com>
2025-10-05 00:53:35 +0600

    fixed all bugs

--- server/app.js
+++ server/app.js
@@ -1,26 +1,45 @@
+// server/app.js
 import express from 'express';
 import { Server } from 'socket.io';
-import { createServer } from "http";
-
-const port = 3000;
+import { createServer } from 'http';
+import cors from 'cors';
 
 const app = express();
 const server = createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173', // Vite dev server
+    methods: ['GET', 'POST'],
+  },
+});
 
+const port = 3000;
 
+// Middleware
+app.use(cors());
+
+// Test route
 app.get('/', (req, res) => {
-  res.send('Hello emam Saimon');
+  res.send('Server is up!');
 });
 
-io.on("connection", (socket) => {
-  console.log("user connected");
-  console.log("Id", socket.id);
-  
+// WebSocket connection
+io.on('connection', socket => {
+  console.log('User connected:', socket.id);
+
+  socket.on('message', data => {
+    console.log(`Message from ${socket.id}:`, data);
+
+    // Broadcast message to all clients
+    io.emit('message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
 });
 
-
-app.listen(port, () => {
-  console.log(`server is running on port: ${port}`);
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
