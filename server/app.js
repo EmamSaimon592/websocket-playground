15bec4979f8b8985bc9988af5ec41a0cf4f12c0d
Emam saimon <likonsaimon123@gmail.com>
2025-10-04 23:52:29 +0600

    websocket

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { Server } from 'socket.io';
+import { createServer } from "http";
+
+const port = 3000;
+
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello emam Saimon');
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  console.log("Id", socket.id);
+  
+});
+
+
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`);
+});
